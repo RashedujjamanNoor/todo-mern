470a5ee1737cbf57327dcb9c8a502dc00758a28e
RashedujjamanNoor <noorrk042@gmail.com>
2025-06-09 23:54:01 +0600

    update homepage

--- src/pages/UpdateUser.jsx
+++ src/pages/UpdateUser.jsx
@@ -3,10 +3,10 @@ import React from "react";
 const UpdateUser = () => {
   return (
     <div className="flex justify-center items-center w-[100vw] h-[100vh] bg-gradient-to-b from-purple-500 to-pink-500">
-      <div className="bg-gray-200 p-7 rounded-2xl">
+      <div className="bg-gray-200 p-7 rounded-2xl min-w-96 h-fit mx-4">
         <h1 className="text-3xl font-bold text-center">User's</h1>
         <hr className="mb-4" />
-        <table className="table-auto">
+        <table className="table-auto w-full text-center">
           <thead>
             <tr>
               <th>Name</th>
